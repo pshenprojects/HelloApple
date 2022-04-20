6c29a578b96100dd6dee3a318eb28c285ee066f7
peteS <pete2shen@yahoo.com>
2022-04-20 11:57:08 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Pete Shen on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Test")
+            .font(.title)
+            .foregroundColor(.primary)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/HelloAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleApp.swift
+//  Shared
+//
+//  Created by Pete Shen on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
